308aa45c8f1b39f485e0ed0e78bb60a8a7a5824d
Michael Wisely <michaelwisely@gmail.com>
2012-11-24 17:41:09 -0600

    Forgot to add cost parameters to function calls

--- division_algorithms.h
+++ division_algorithms.h
@@ -1,18 +1,19 @@
 Binary multiplicative_division(const Binary& a, const Binary& b, unsigned int& cost) {
-  Binary ONE(a.size);
-  ONE = 1;
+  Binary one(a.size), zero(a.size);
+  zero = 0;
+  one = 1;
 
   Binary f_i = F0;
   Binary a_i = a;
   Binary b_i = b;
   Binary b_i_complement;
 
-  while(b_i != ONE) {
+  while(b_i != one) {
 	b_i_complement = b_i;
-	b_i_complement.complement();
-	f_i = add(b_i_complement, ONE);
-	a_i = mul(a_i, f_i);
-	b_i = mul(b_i, f_i);
+	b_i_complement.complement(cost);
+	f_i = add(b_i_complement, one, false, cost);
+	a_i = mul(a_i, f_i, cost);
+	b_i = mul(b_i, f_i, cost);
   }
 
   return a_i;
