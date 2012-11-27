48a0ab20a5e379b7c3004ff1b4f5a539586121e7
Michael Wisely <michaelwisely@gmail.com>
2012-11-26 23:54:05 -0600

    Cleaned up division_algorithms.
    Removed comments and fixed up style

--- division_algorithms.h
+++ division_algorithms.h
@@ -4,6 +4,14 @@ using namespace std;
 
 static const int ITERLIMIT = 5;
 
+
+/**
+ * Performs a / b = ? using multiplicative division method
+ * @param a [in] Left hand side
+ * @param b [in] Right hand side
+ * @param cost [in/out] Cost to perform operation
+ * @return Binary value with the result
+ */
 Binary multiplicative_division(const Binary& a, const Binary& b, unsigned int& cost) {
   int size = a.get_size();
   Binary one(size);
@@ -12,7 +20,7 @@ Binary multiplicative_division(const Binary& a, const Binary& b, unsigned int& c
   }
   one.set_digit(size-1, 0);
   one.set_decimal(size-1);
-  
+
   Binary f_i = one;
   Binary a_i = a;
   Binary b_i = b;
@@ -36,6 +44,7 @@ Binary multiplicative_division(const Binary& a, const Binary& b, unsigned int& c
   return a_i;
 }
 
+
 /**
  * Performs a / b = ? using divisor reciprocation method
  * @param a [in] Left hand side
@@ -44,79 +53,63 @@ Binary multiplicative_division(const Binary& a, const Binary& b, unsigned int& c
  * @return Binary value with the result
  */
 Binary divisor_reciprocation(const Binary& aP, const Binary& bP, unsigned int& cost) {
+  Binary a = aP;
+  Binary b = bP;
 
-	Binary a = aP;
-	Binary b = bP;
+  if(b.toDouble() < 0.5 || b.toDouble() >= 1)
+  {
+    double bVal = b.toDouble();
+    while(bVal < 0.5)
+    {
+      b = b << 1;
+      b.decimal++;
+      a = a << 1;
+      a.decimal++;
+      bVal *= 2;
+    }
 
-	if(b.toDouble() < 0.5 || b.toDouble() >= 1)
-	{
-//		cout << "WARNING: divisor(B) is " << b << " outside of 1/2 <= B < 1" << endl;
-		double bVal = b.toDouble();
-		while(bVal < 0.5)
-		{
-		  b = b << 1;
-		  b.decimal++;
-		  a = a << 1;
-		  a.decimal++;
-		  bVal *= 2;
-		}
+    while(bVal >= 1)
+    {
+      b = b >> 1;
+      b.decimal--;
+      a = a >> 1;
+      a.decimal++;
+      bVal /= 2;
 
-		while(bVal >= 1)
-		{
-		  b = b >> 1;
-		  b.decimal--;
-		  a = a >> 1;
-		  a.decimal++;
-		  bVal /= 2;
+    }
+  }
 
-		}
-	}
+  int size = max(a.get_size(), b.get_size());
+  Binary x_0(size, b.get_decimal());
+  x_0 = a.resize(size);
 
-	int size = max(a.get_size(), b.get_size());
-	Binary x_0(size, b.get_decimal());
-	x_0 = a.resize(size);
+  Binary a_0 = b.resize(size); 
 
-	Binary a_0 = b.resize(size); //mul(b.resize(size), x_0.resize(size), cost).resize(size);
+  Binary x_i = x_0, a_i = a_0;
+  Binary TWO(size, b.get_decimal());
+  TWO = "010.0";
+  TWO = TWO.resize(size);
 
+  while(abs(x_i.toDouble() - a.toDouble() / b.toDouble()) >= pow(2, -size + 3) &&
+        x_i.get_size() <= size) // Division overflow
+  {
+    // Perform the operations in parallel
+    unsigned int costX = cost, costA = cost;
 
-	Binary x_i = x_0, a_i = a_0;
-	Binary TWO(size, b.get_decimal());
-	TWO = "010.0";
-	TWO = TWO.resize(size);
+    Binary sub_x = sub(TWO, a_0, costX);
+    Binary sub_a = sub(TWO, a_0, costA);
 
-//
-//	std::cout << "Iteration #" << 0 << endl;
-//	std::cout << "x_" << 0 << " = " << x_0 << endl;
-//	std::cout << "a_" << 0 << " = " << a_0 << endl;
+    x_i = mul(x_0, sub_x, costX);
+    a_i = mul(a_0, sub_a, costA);
+    cost = max(costX, costA);
 
-	while(abs(x_i.toDouble() - a.toDouble() / b.toDouble()) >= pow(2, -size + 3) &&
-			x_i.get_size() <= size) // Division overflow
-	{
-		// Perform the operations in parallel
-		unsigned int costX = cost, costA = cost;
+    x_i = x_i.resize(size);
+    a_i = a_i.resize(size);
 
-		Binary sub_x = sub(TWO, a_0, costX);
-		Binary sub_a = sub(TWO, a_0, costA);
+    // x_0 is really x_i-1 for the purposes of this loop.
+    x_0 = x_i;
+    a_0 = a_i;
+  }
 
-//		std::cout << "Iteration #" << i + 1 << endl;
-//
-//		cout << TWO << " - " << a_0 << " = " << sub_x << endl;
-		x_i = mul(x_0, sub_x, costX);
-//		cout << x_0 << " * " << sub_x << " = " << (x_i) << endl;
-		a_i = mul(a_0, sub_a, costA);
-//		cout << a_0 << " * " << sub_a << " = " << (a_i) << endl;
-		cost = max(costX, costA);
-
-		x_i = x_i.resize(size);
-		a_i = a_i.resize(size);
-
-//		std::cout << "x_" << i + 1 << " = " << x_i << endl;
-//		std::cout << "a_" << i + 1 << " = " << a_i << endl;
-
-		// x_0 is really x_i-1 for the purposes of this loop.
-		x_0 = x_i;
-		a_0 = a_i;
-	}
-
-	return x_i;
+  return x_i;
 }
